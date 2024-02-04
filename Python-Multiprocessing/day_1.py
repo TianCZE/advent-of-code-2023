cfb3fb9671c42789ce31e88ccb5c72ac06b8456d
Sebastinán Novák <novak.basty@gmail.com>
2024-02-04 22:57:08 +0100

    modified Python

--- Python-Multiprocessing/day_1.py
+++ Python-Multiprocessing/day_1.py
@@ -1,12 +1,8 @@
-def filter_out_letters(content: str):
-    result = ""
-    for char in content:
-        if char.isalpha:
-            result += char
-    return content
+def filter_out_letters(string: str) -> str:
+    return ''.join(filter(lambda s: str.isdigit(s), string))
 
 
 if __name__ == "__main__":
-    with open('day_1_input.txt', 'r') as input:
-        content = input.read()
+    with open('day_1_input.txt', 'r') as file:
+        content = file.read()
         print(filter_out_letters(content))
